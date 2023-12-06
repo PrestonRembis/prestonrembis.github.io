0a1c4a66131c5ff43cdb5c429e00f98c349b150a
Preston Rembis <pr395420@ohio.edu>
2023-12-06 00:25:29 -0500

    Loading indicator on submit

--- script2.js
+++ script2.js
@@ -1,9 +1,13 @@
 const userForm = document.getElementById('user-form');
+const loading = document.getElementById('loading'); // Get the loading element
 
 userForm.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent the default form submission
 
   const formData = new FormData(userForm);
+
+  // Show loading text when form is submitted
+  loading.style.display = 'block';
 
   fetch('https://script.google.com/macros/s/AKfycbwNt7fkwwMYnsN7FLx2_o8T8iueZEVFGDiUVxpaidl8JiWNZy-_Zr1xVq6PHedRe08/exec', {
     method: 'POST',
@@ -11,13 +15,14 @@ userForm.addEventListener('submit', (event) => {
   })
   .then(response => {
     console.log('Form submitted!', response);
-    // Optionally, you can perform actions upon successful form submission
-    // For example, display a success message or redirect the user back to the contact page
-    // Replace window.location.href with your desired redirect URL
+    // Hide loading text upon successful submission
+    loading.style.display = 'none';
+
     window.location.href = 'contact.html';
   })
   .catch(error => {
     console.error('Error submitting form:', error);
     // Handle any errors that occurred during form submission
+    loading.style.display = 'none'; // Hide loading text in case of errors
   });
 });
